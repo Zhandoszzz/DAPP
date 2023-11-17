0bcf8c9e89d890fe2f74d0e2fbf71c7217a50c31
zhandos <zhandosshakimurat@gmail.com>
2023-11-17 10:04:08 +0600

    add friends

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.addEventListener('load', async () => {
         console.error('No Ethereum provider detected');
     }
 
-    const contractAddress = '0xffd1E4861ed1D1814dE7a3bA33C2691D331C83E1'; // Replace with your actual contract address
+    const contractAddress = '0x47B087B83cD9578C780fAF8c4A801192f4D3A688'; // Replace with your actual contract address
     const contractABI = [
         {
             "inputs": [
@@ -194,13 +194,14 @@ window.addEventListener('load', async () => {
                 result.forEach((user, index) => {
                     const { name, accountAddress } = user;
                     const userInfoElement = document.createElement('div');
+                    userInfoElement.classList.add('user-card');                    
                     userInfoElement.innerHTML = `
-            <h2>User ${index + 1}</h2>
-            <p>Name: ${name}</p>
-            <p>Address: ${accountAddress}</p>
-            <hr>
+      <h2 class="user-name">${name}</h2>
+      <p class="user-address">${accountAddress}</p>
+      <button class="add-friend-btn" onclick="addFriend('${accountAddress}','${name}')" >Add Friend</button>
           `;
                     allUsersElement.appendChild(userInfoElement);
+                    console.log(allUsersElement)
                 });
             } else {
                 console.error('Error fetching users:', error);
@@ -215,7 +216,7 @@ window.addEventListener('load', async () => {
 async function createUser() {
 
     try {
-        
+
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
         const name = document.querySelector('#nameInput').value
@@ -230,13 +231,63 @@ async function createUser() {
         console.log('User created:', tx);
         alert('User created successfully!');
 
+        window.location.href = 'chat.html';
+
     } catch (error) {
-        // Handle errors
-        console.error('Error creating user:', error);
+
+        console.error('Error creating user:', error.message);
+
         alert('Error creating user. Please check the console.');
     }
 }
 
+//check
+async function addFriend(friendAddress, friendName) {
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const currentAccount = accounts[0];
+
+        const result = await contract.methods.addFriend(friendAddress, friendName).send({ from: currentAccount });
+
+        console.log('Friend added:', result);
+    } catch (error) {
+        console.error('Error adding friend:', error);
+    }
+}
+
+//check
+async function getFriends() {
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const currentAccount = accounts[0];
+
+        const friends = await contract.methods.getFriends().call({ from: currentAccount });
+
+        console.log('List of Friends:', friends);
+    } catch (error) {
+        console.error('Error retrieving friends:', error);
+    }
+}
+//check
+async function readMessages(friendAddress) {
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const currentAccount = accounts[0];
+
+        // Call the contract method 'readMessage'
+        const messages = await contract.methods.readMessage(friendAddress).call({ from: currentAccount });
+
+        // Handle the retrieved messages
+        console.log('Messages with Friend:', messages);
+        // You can perform additional actions or UI updates with the messages
+    } catch (error) {
+        // Handle errors
+        console.error('Error reading messages:', error);
+    }
+}
+
+
+
 function redirectToMainPage() {
-    window.location.href = 'chat.html';
+    window.location.href = 'landing.html';
 }
